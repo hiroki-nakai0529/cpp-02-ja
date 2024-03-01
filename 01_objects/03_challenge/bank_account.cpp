d998d8c314270439952d549b5bf33a2445f9f84e
hiroki-nakai0529 <hiroki.nakai@woven-planet.global>
2024-03-01 13:25:47 +0900

    add challenge 3

--- 01_objects/03_challenge/bank_account.cpp
+++ 01_objects/03_challenge/bank_account.cpp
@@ -1,1 +1,28 @@
-// コードを入力してください
+#include "bank_account.h"
+
+BankAccount::BankAccount(const double& init_balance) : balance(init_balance){};
+
+
+void BankAccount::deposit(const double& deposit){
+    double new_balance =  this->balance - deposit;
+    if(deposit > 0){
+        this->balance =  this->balance + deposit;
+    }
+    std::cout << this->balance << std::endl;
+}
+
+void BankAccount::withdraw(const double& withdraw){
+    double new_balance =  this->balance - withdraw;
+    if(new_balance < 0 ){
+        std::cerr<< "Insufficient balance!" << std::endl;
+    }
+    else if(withdraw > 0){
+        this->balance = new_balance;
+        std::cout << this->balance << std::endl;
+    }
+}
+
+double BankAccount::get_balance(){
+    std::cout << this->balance << std::endl;
+    return this->balance;
+}
--- 01_objects/03_challenge/bank_account.h
+++ 01_objects/03_challenge/bank_account.h
@@ -1,1 +1,12 @@
-// コードを入力してください
+#include <iostream>
+#include <string>
+
+class BankAccount {
+    public:
+        BankAccount(const double& init_balance);
+        void deposit(const double& deposit);
+        void withdraw(const double& withdraw);
+        double get_balance();
+    private:
+        double balance;
+};
